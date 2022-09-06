6426943c71b3949a75ea1c3166720d27c80019dd
赵之琛 <zhaozc@shanshan-business.com>
2022-09-06 12:50:36 +0800

    更换网页描述和LOGO

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  publicPath : process.env.NODE_ENV === 'production'  ? "/h5/todayMeal"  : "/"
+  publicPath : process.env.NODE_ENV === 'production'  ? "/todayMeal"  : "/",
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "今天吃点啥"
+      return args;
+    })
+  }
 })
